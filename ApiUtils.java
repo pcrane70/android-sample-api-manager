eaf7fdbc4f0703ba9f470a44bd16864d0ce620ad
CitiOpenBanking <OpenBanking@citi.com>
2017-09-23 09:11:41 -0400

    initial Upload

---
+++ ApiUtils.java
@@ -0,0 +1,11 @@
+
+
+public class ApiUtils {
+
+
+        public static final String BASE_URL = "https://sandbox.apihub.citi.com/gcb/api/";
+
+        public static OAuthService getSOService() {
+            return RetrofitClient.getClient(BASE_URL).create(OAuthService.class);
+        }
+}
---
+++ OAuthRequestBody.java
@@ -0,0 +1,17 @@
+
+import com.google.gson.annotations.SerializedName;
+
+public class OAuthRequestBody {
+
+    @SerializedName("grant_type")
+    private String grant_type;
+    @SerializedName("scope")
+    private String scope;
+
+    public OAuthRequestBody(String grant_type, String scope) {
+        this.grant_type = grant_type;
+        this.scope = scope;
+    }
+
+
+}
